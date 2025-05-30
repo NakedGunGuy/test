d0c535d26ff8064c679e368d9bafa585f203339f
perko <klemen.perko@movepeople.ch>
2025-05-30 14:42:44 +0200

    htmx, preload

--- views/home.php
+++ views/home.php
@@ -11,11 +11,15 @@
 
     <link rel="preload" href="/css/output.css" as="style">
     <link rel="stylesheet" href="/css/output.css" media="all">
+
+    <script src="/js/htmx.min.js"></script>
+    <script src="/js/preload.js"></script>
 </head>
 
-<body>
+<body hx-ext="preload" data-htmx-log-level="debug">
 	<h1>Welcome to <?= htmlspecialchars($appName) ?></h1>
     <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
+    <a href="/user/1" preload="mouseover">About</a>
 </body>
 
 </html>
