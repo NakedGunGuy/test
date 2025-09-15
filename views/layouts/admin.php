a4612f6d97202bcc7ce9803083e0222e3f0f588f
perko <klemen.perko@movepeople.ch>
2025-09-15 12:53:07 +0200

    tests

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?= section('title', htmlspecialchars($_ENV['APP_NAME'])) ?></title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/default.css" media="all">
 
     <?= section('css') ?>
